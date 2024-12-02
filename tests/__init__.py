763115b28b5fc0b6d4195354f7eb55202bb463ed
Alex Lukens <alex.lukens@canonical.com>
2024-12-01 22:23:59 -0500

    removed separate testing logger

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# Content for /snap_python/tests/__init__.py
-import logging
-
-logger = logging.getLogger("snap_python.tests")
-logger.setLevel(logging.DEBUG)
-logger.handlers.clear()
-logger.addHandler(logging.StreamHandler())
-logger.addHandler(logging.FileHandler("snap_python_testing.log"))
