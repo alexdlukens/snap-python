3986d13c0eefe1fb662548524ac2a1437026c146
Alexander Lukens <alexdlukens@gmail.com>
2025-10-02 20:28:32 -0400

    set default log level to info

--- src/snap_python/__init__.py
+++ src/snap_python/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-log_level = os.getenv("SNAP_PYTHON_LOG_LEVEL", "DEBUG").upper()
-numeric_level = getattr(logging, log_level, logging.DEBUG)
+log_level = os.getenv("SNAP_PYTHON_LOG_LEVEL", "INFO").upper()
+numeric_level = getattr(logging, log_level, logging.INFO)
 logger = logging.getLogger("snap_python")
 logger.setLevel(numeric_level)
